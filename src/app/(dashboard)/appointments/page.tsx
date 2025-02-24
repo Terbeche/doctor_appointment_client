ca06d96b31fec707e01e80d9222d246529e613e8
mostefa <termus96@gmail.com>
2025-02-24 18:39:28 +0100

    Add basic appointments page

---
+++ src/app/(dashboard)/appointments/page.tsx
@@ -0,0 +1,10 @@
+import DashboardLayout from '@/components/layout/DashboardLayout';
+import Appointments from '@/components/Appointments';
+
+export default function AppointmentsPage() {
+  return (
+    <DashboardLayout>
+      <Appointments />
+    </DashboardLayout>
+  );
+}
